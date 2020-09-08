e394b8f0111839a235cbb3011e0fe5412f351de6
João Henrique de Oliveira Júnior <joao.holiveira@totvs.com.br>
2020-09-08 17:28:38 -0300

    instando o ngx-markdown

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { PoMenuItem } from '@po-ui/ng-components';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(public httpClient: HttpClient) {}
+
+
+  public ngOnInit(): void {
+    // this.httpClient.get('')
+    //   .subscribe(
+    //     sucesso => console.log(sucesso),
+    //     erro => console.log(erro)
+    //   );
+  }
 
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) }
+    { label: 'Bem vindo!', action: this.onClick.bind(this) }
   ];
 
   private onClick() {
-    alert('Clicked in menu item')
+    alert('Clicked in menu item');
   }
 
 }
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoAvatarModule, PoPageModule, PoToolbarModule, PoMenuModule } from '@po-ui/ng-components';
+import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     PoAvatarModule,
     PoPageModule,
     PoToolbarModule,
     PoMenuModule,
-    PoAvatarModule
+    PoAvatarModule,
+    MarkdownModule.forRoot({
+      sanitize: SecurityContext.NONE,
+      markedOptions: {
+        provide: MarkedOptions,
+        useValue: {
+          gfm: true,
+          breaks: true,
+          pedantic: true,
+          smartLists: true,
+          smartypants: true,
+        }
+      },
+    })
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
